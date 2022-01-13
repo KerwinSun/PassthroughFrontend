85fe3a419bf4eb074e4b03b9b63419195e1cb9d5
unknown <kerwin10010@gmail.com>
2022-01-13 13:02:42 +1300

    console results added

--- src/pages/Console.js
+++ src/pages/Console.js
@@ -1,33 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const baseURL = "http://143.110.178.16:9000/passthrough/api/v1/executeCommand?cmd="
+const baseResultURL = "http://143.110.178.16:9000/passthrough/api/v1/getCommandOutput"
 const Console = () => {
-  const [name, setName] = useState("");
+  
+  const [command, setCommandBar] = useState("");
+  const [secretKey, setSecretKey] = useState("");
+  const [result, setResultArea] = useState("");
 
   const submitCommand = (event) => {
     event.preventDefault();
-    axios.get(baseURL+name).then((response) => {
-      console.log(response.data);
+    setResultArea(oldresult => oldresult + "-------------------------------------------------------------------- \n")
+    axios.get(baseURL+command).then((response) => {
+      setResultArea(oldresult => oldresult + "results for command: " + response.data?.command + "\n")
     });
   }
 
-  const getCommand = (event) => {
+  const clearconsole = (event) => {
     event.preventDefault();
-    alert(`The name you entered was: ${name}`)
+    setResultArea("")
   }
 
+  const getCommandResult = async () => {
+    axios.get(baseResultURL).then((response) => {
+      setResultArea(oldresult => oldresult + response.data?.result)
+    });
+  };
+
+  useEffect(() => {
+    setInterval(getCommandResult, 1000);
+  }, []);
+
   return (
+    <html>
     <form onSubmit={submitCommand}>
-      <label>Enter your command:
+      <div>
+      <label>Enter secret key: </label>
+        <input 
+          type="password" 
+          value={secretKey}
+          onChange={(e) => setSecretKey(e.target.value)}
+        />
+        <br />
+        <label>Enter your command: </label>
         <input 
           type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={command}
+          onChange={(e) => setCommandBar(e.target.value)}
         />
-      </label>
       <input type="submit" />
-    </form>
+      </div>
+      </form>
+
+      <div>
+      <label>command results:</label>
+        <div>
+        <textarea 
+          type="textarea" 
+          style={{width: "800px", height: "500px"}}
+          value={result}
+        />
+        </div>
+      </div>
+
+      <form onSubmit={clearconsole}>
+      <div>
+      <input type="submit" value="clear console" />
+      </div>
+      </form>
+      </html>
   )
 };
 
